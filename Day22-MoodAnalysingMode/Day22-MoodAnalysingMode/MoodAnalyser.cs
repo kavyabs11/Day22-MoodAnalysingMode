df594e7535cac45f6ed133d6ec706a3b824309d4
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-12 13:12:37 +0530

    MoodAnalyser

---
+++ Day22-MoodAnalysingMode/Day22-MoodAnalysingMode/MoodAnalyser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day22_MoodAnalysingMode
+{
+
+    public class MoodAnalyser
+    {
+        public void analyseMood(string message)
+        {
+            if (message.ToUpper().Contains("SAD"))
+                Console.WriteLine("The user mood is SAD");
+            else
+                Console.WriteLine("The user mood is HAPPY");
+        }
+    }
+}
+
--- Day22-MoodAnalysingMode/Day22-MoodAnalysingMode/Program.cs
+++ Day22-MoodAnalysingMode/Day22-MoodAnalysingMode/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Welcome to MoodAnalyser MSTesting");
+            Console.WriteLine("Enter your mood to check");
+            string mood = Console.ReadLine();
+            MoodAnalyser obj = new MoodAnalyser();
+            obj.analyseMood(mood);
         }
     }
 }
