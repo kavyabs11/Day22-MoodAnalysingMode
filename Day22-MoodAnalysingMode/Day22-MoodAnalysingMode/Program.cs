69c5099718022393fa28b3b92592bbd0244648c7
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-12 12:59:43 +0530

    Analysing mood from the given message

---
+++ Day22-MoodAnalysingMode/Day22-MoodAnalysingMode/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day22_MoodAnalysingMode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
