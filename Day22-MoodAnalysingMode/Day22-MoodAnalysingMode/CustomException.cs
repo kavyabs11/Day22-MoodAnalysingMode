461cb80ca8c9c16b4ddabbb7e74b223fc85aa5f0
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-14 18:54:00 +0530

    MoodAnalyser using Reflection Test Case

--- Day22-MoodAnalysingMode/Day22-MoodAnalysingMode/CustomException.cs
+++ Day22-MoodAnalysingMode/Day22-MoodAnalysingMode/CustomException.cs
@@ -11,7 +11,9 @@ namespace Day22_MoodAnalysingMode
         public enum ExceptionType
         {
             NULL_MESSAGE,
-            EMPTY_MESSAGE
+            EMPTY_MESSAGE,
+            NO_SUCH_CLASS,
+            NO_SUCH_METHOD
         }
         private readonly ExceptionType Type;
         public CustomException(ExceptionType type, string message) : base(message)
